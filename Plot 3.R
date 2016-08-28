7265968b99ad5f6b5c1660849b60dbb2e68d5cf4
PengLi8147 <pyrophosphate8147@gmail.com>
2016-08-27 20:26:26 -0400

    Add files via upload

---
+++ Plot 3.R
@@ -0,0 +1,66 @@
+#####################################################################################################################################
+# Coursera Project 
+# Peer Graded Assignment: Exploratory Data Analysis Course Project
+# Plot3.R
+# Name: Peng Li
+#####################################################################################################################################
+
+# R Script starts here below:
+
+####################################################################################################################################
+
+# Download the zip file
+rm(list = ls()) # clean up the working environment
+fileurl<-"https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(fileurl,destfile = "Dataset.zip")
+
+# Read the text file into R
+electric <- read.table(unz("Dataset.zip", "household_power_consumption.txt"), sep = ";", header = TRUE)
+
+# Load the relevant R packages
+library(tidyr)
+library(dplyr)
+library(lubridate)
+
+electric <- tbl_df(electric) # read the data in tbl data frame format 
+
+# Select only two dates: 2007 Feb 1st and 2007 Feb 2nd
+elec_power <- filter(electric, Date == "1/2/2007" | Date == "2/2/2007" )
+
+# Convert the Time Variable in Elec_Power in time format
+elec_power$Time <- strptime(paste(elec_power$Date, elec_power$Time), "%e/%m/%Y %H:%M:%S")
+
+# Convert the Date Variable to a Date class
+elec_power$Date <- strptime(elec_power$Date, "%e/%m/%Y")
+elec_power$Date <- as.Date(elec_power$Date)
+
+
+# Change the class of other variables from factor to numeric
+elec_power$Global_active_power <- as.numeric(levels(elec_power$Global_active_power))[elec_power$Global_active_power]
+elec_power$Global_reactive_power <- as.numeric(levels(elec_power$Global_reactive_power))[elec_power$Global_reactive_power]
+elec_power$Voltage <- as.numeric(levels(elec_power$Voltage))[elec_power$Voltage]
+elec_power$Global_intensity <- as.numeric(levels(elec_power$Global_intensity))[elec_power$Global_intensity]
+elec_power$Sub_metering_1 <- as.numeric(levels(elec_power$Sub_metering_1))[elec_power$Sub_metering_1]
+elec_power$Sub_metering_2 <- as.numeric(levels(elec_power$Sub_metering_2))[elec_power$Sub_metering_2]
+elec_power$Sub_metering_3 <- as.numeric(levels(elec_power$Sub_metering_3))[elec_power$Sub_metering_3]
+
+
+# Load the "grDevices" R Package so as to save the plotted PNG graph to local directory
+require(grDevices)
+library(grDevices)
+
+### Ready to make the plots of the variables 
+
+
+# Plot 3
+
+# plot sub_metering_1 first and add the additiona two lines for sub_metering_2 and sub_metering_3
+png(filename = "Plot 3.png", width = 480, height = 480, units = "px")
+plot(elec_power$Time, elec_power$Sub_metering_1, type = "l", ylab = "Global Active Power (Kilowatts)", xlab = "")
+lines(elec_power$Time, elec_power$Sub_metering_2, col = "red")
+lines(elec_power$Time, elec_power$Sub_metering_3, col = "blue")
+legend("topright", lwd = 1, col = c("black", "blue", "red"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+dev.off()
+
+
+
